24c38a50a14bda1806784a3291002da0e42b478c
Alamin Khan <alamin@Alamins-Mini.lan>
2022-08-02 23:00:06 +0300

    icons

--- src/components/ServiceDetails/ServiceDetails.jsx
+++ src/components/ServiceDetails/ServiceDetails.jsx
@@ -5,7 +5,7 @@ import './ServiceDetails.css'
 const ServiceDetails = ({service}) => {
   return (
     <div className="col-md-4 text-center">
-      <img style={{height: '50px'}} src={service.img} alt=""/>
+      <img style={{height: '70px'}} src={service.img} alt=""/>
       <h5 className="mt-4 mb-4 name">{service.name}</h5>
       <p className="description">{service.description}</p>
       <p className="benefits">Business Benefits -{service.benefit}</p>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,47 +2,50 @@ import React, { useContext } from "react";
 import "./Services.css";
 import { themeContext } from "../../Context";
 import ServiceDetails from "../ServiceDetails/ServiceDetails";
-import fluoride from "../../img/fluoride.png";
-import cavity from "../../img/cavity.png";
-import whitening from "../../img/whitening.png"
+import frontend from "../../img/front.png";
+import backend from "../../img/database.png";
+import configuration from "../../img/settings.png";
+import quality from "../../img/quality.png";
+import design from "../../img/design.png";
+import api from "../../img/api.png";
 
 const serviceData=[
   {
       name:'Front-End Development',
-      img:fluoride,
+      img:frontend,
       description:'I have created awesome front end web application with HTML CSS/SCSS ,Bootstrap JavaScript , NodeJs and reactJs',
       benefit:"Quick  , Responsive  and User-driven result"
 
   },
   {
       name:'Back-End Development',
-      img:cavity,
+      img:backend,
       description:'Proficiently handled application which i run on server main goal is function seamlessly and fulfill customer expectations',
       benefit:"Less cost with enhanced capacity on main focused function "
 
   },
   {
       name:'API Integration',
-      img:whitening,
+      img:api,
       description:'With adequate knowledge with API integration i develop smooth and seamless function',
       benefit:" Great application with higher performance"
   },
   {
-    name:'Database configuration',
-    img:fluoride,
+    name:'Database Configuration',
+    img:configuration,
     description:'I have a great understanding of linking database with back-end language',
     benefit:"Easy updatable data with advance search options"
 },
 {
     name:'Quality Analysis',
-    img:cavity,
+    img:quality,
     description:'My main priority is always delivery highest quality of product to my customer without bugs ,errors and harmful element',
     benefit:"Application safety without unexpected breakdown"
 
 },
 {
   name:'UI/UX Design',
-  img:cavity,
+  img:design,
   description:'With experience of different type of design applications i can ensure height experiences with user friendly design ',
   benefit:"Responsive and user friendly design"
 }
